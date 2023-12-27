962d45e61372ef98a7bc08c98832ed8a64998fc7
lilman109 <akira.yoshidasjsu@gmail.com>
2023-12-27 09:31:10 +0900

    implmented pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import AuthLayout from "./_auth/AuthLayout";
 import SigninForm from "./_auth/SigninForm";
 import SignupForm from "./_auth/SignupForm";
 import { Routes, Route } from "react-router-dom";
-import Home from "./_root/pages/Home";
 import RootLayout from "./_root/RootLayout";
 import { Toaster } from "./components/ui/toaster";
+import { Home, Explorer, AllUsers, Saved, CreatePost, EditPost, PostDetails, Profile, UpdateProfile } from "./_root/pages/";
 
 const App = () => {
   return (
@@ -17,6 +17,14 @@ const App = () => {
         </Route>
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explorer" element={<Explorer />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
